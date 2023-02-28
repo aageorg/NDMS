01b7a4d36ef7306c398894e0944cf39acd0c469b
Nastasia Georg <nastasia@ndl.ru>
2023-02-28 21:18:25 +0300

    Fixes based on code review #1

--- main.go
+++ main.go
@@ -27,21 +27,15 @@ type Worker struct {
 	waiters     Queue
 }
 
-// PUT request object
-type PutRequest struct {
-	queueName string
-	value     string
-}
-
 // GetRequest is created in the http handler function on every GET request.
 type GetRequest struct {
-	QueueName                string              // requested queue name
-	DataChannel              chan string         // channel where to send requested data
-	writer                   http.ResponseWriter // http.ResponseWriter to reply
-	timeout                  int                 // timeout in milliseconds
-	done, expired, stoptimer chan bool           // channels for control of GetRequest's state
-	stopped                  bool                // Indicates if the GetRequest active or not
-	mu                       sync.Mutex
+	QueueName   string              // requested queue name
+	DataChannel chan string         // channel where to send requested data
+	writer      http.ResponseWriter // http.ResponseWriter to reply
+	timeout     int                 // timeout in milliseconds
+	done        chan bool           // channels for control of GetRequest's state
+	stopped     bool                // Indicates if the GetRequest active or not
+	mu          sync.Mutex
 }
 
 // Worker's data storage
@@ -69,7 +63,7 @@ func (b *Broker) PutData(queue_name string, value string) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	if _, ok := b.workers[queue_name]; !ok {
-		w := Worker{}.New()
+		w := makeWorker()
 		b.workers[queue_name] = &w
 		go w.Start()
 	}
@@ -83,7 +77,7 @@ func (b *Broker) Route(req *GetRequest) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	if _, ok := b.workers[req.QueueName]; !ok {
-		w := Worker{}.New()
+		w := makeWorker()
 		b.workers[req.QueueName] = &w
 		go w.Start()
 	}
@@ -102,36 +96,11 @@ func (req *GetRequest) Feed(s string) bool {
 	return false
 }
 
-// Wait starts timeout countdown.
-func (req *GetRequest) Wait() {
-	if req.timeout == 0 {
-		return
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(req.timeout)*time.Millisecond)
-	defer cancel()
-	select {
-	case <-req.stoptimer:
-		return
-	case <-ctx.Done():
-		req.expired <- true
-	}
-}
-
 // Stop sets GetRequest.stopped to true.
 func (req *GetRequest) Stop() {
 	req.mu.Lock()
 	req.stopped = true
 	req.mu.Unlock()
-}
-
-// PauseTimer stops the timeout countdown
-func (req *GetRequest) PauseTimer() {
-	if req.timeout > 0 {
-		select {
-		case req.stoptimer <- true:
-		default:
-		}
-	}
 }
 
 // Reply sends Data via http
@@ -153,8 +122,8 @@ func (q *Queue) Add(req *GetRequest) {
 	q.mu.Unlock()
 }
 
-// New returns a new Worker
-func (w Worker) New() Worker {
+// A Worker constructor
+func makeWorker() Worker {
 	return Worker{
 		getRequests: make(chan *GetRequest, 10),
 		waiters:     Queue{qu: []*GetRequest{}},
@@ -192,7 +161,7 @@ func (w *Worker) ServeWaiter(s string) bool {
 	w.waiters.mu.Lock()
 	defer w.waiters.mu.Unlock()
 	for i := 0; i < len(w.waiters.qu); i++ {
-		w.waiters.qu[i].PauseTimer()
+		//		w.waiters.qu[i].PauseTimer()
 		if !w.waiters.qu[i].Feed(s) {
 			w.waiters.qu = append(w.waiters.qu[:i], w.waiters.qu[i+1:]...)
 			i--
@@ -217,6 +186,73 @@ func (w *Worker) Start() {
 	}
 }
 
+func HandleRequest(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	url_path_slice := strings.Split(r.URL.Path, "/")
+	queue_name := url_path_slice[len(url_path_slice)-1]
+	// If queue name is empty, it is a bad request (400)
+	if queue_name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(nil)
+		return
+	}
+	r.ParseForm()
+
+	if r.Method == "PUT" {
+		if val, ok := r.Form["v"]; ok == true && val[0] != "" {
+			br.PutData(queue_name, val[0])
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write(nil)
+		}
+		return
+	}
+
+	if r.Method == "GET" {
+		var timer int
+		if val, ok := r.Form["timeout"]; ok == true && val[0] != "" {
+			if t, err := strconv.Atoi(val[0]); err == nil && t > 0 {
+				timer = t * 1000
+			}
+		}
+		me := &GetRequest{
+			QueueName:   queue_name,
+			DataChannel: make(chan string),
+			timeout:     timer,
+			writer:      w,
+			done:        make(chan bool),
+			stopped:     false,
+		}
+		// Send GetRequest to Broker's common channel for all GetRequests
+		br.Route(me)
+		// Start timeout countdown for the GetRequest
+		ctx := context.Background()
+		var cancel context.CancelFunc
+		if me.timeout > 0 {
+			ctx, cancel = context.WithTimeout(ctx, time.Duration(me.timeout)*time.Millisecond)
+			defer cancel()
+		}
+		// this select awaiting one of possible result of the GetRequest's processing
+		select {
+		case response := <-me.DataChannel: 		// in case of data received, reply 200 OK and send data
+			me.Reply(response)
+		case <-me.done: 				// in case of no data was received, reply 404
+			me.Decline()
+		case <-ctx.Done(): 				// in case of time is up, check DataChannel to avoid lost of the data
+			select { 				// when data was alreday retrieved at the moment of expiration,
+			case response := <-me.DataChannel: 	// then set GetRequest.stopped = true and reply 404
+				me.Reply(response)
+			default:
+				me.Stop()
+				me.Decline()
+			}
+		case <-r.Context().Done(): 			// in case of sudden disconnection, set GetRequest.stopped = true
+			me.Stop()
+		}
+	}
+}
+
 var br Broker
 
 func main() {
@@ -225,81 +261,8 @@ func main() {
 		workers:     make(map[string]*Worker),
 		getRequests: make(chan *GetRequest),
 	}
-	// putRequests is the channel where data came with PUT requests is sent
-	putRequests := make(chan PutRequest, 50)
-	defer close(putRequests)
-	// Route every GetRequest to a suitable Worker:
-	go func() {
-		for req := range br.getRequests {
-			br.Route(req)
-		}
-	}()
-	// Send all incoming data from putRequests to a suitable Worker
-	go func() {
-		for req := range putRequests {
-			br.PutData(req.queueName, req.value)
-		}
-	}()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		defer r.Body.Close()
-
-		url_path_slice := strings.Split(r.URL.Path, "/")
-		q := url_path_slice[len(url_path_slice)-1]
-		// If queue name is empty, it is a bad request (400)
-		if q == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write(nil)
-			return
-		}
-		r.ParseForm()
-
-		if r.Method == "PUT" {
-			if val, ok := r.Form["v"]; ok == true && val[0] != "" {
-				putRequests <- PutRequest{q, val[0]}
-			} else {
-				w.WriteHeader(http.StatusBadRequest)
-				w.Write(nil)
-			}
-			return
-		}
-
-		if r.Method == "GET" {
-			var timer int
-			if val, ok := r.Form["timeout"]; ok == true && val[0] != "" {
-				if t, err := strconv.Atoi(val[0]); err == nil && t > 0 {
-					timer = t * 1000
-				}
-			}
-			me := &GetRequest{
-				QueueName:   q,
-				DataChannel: make(chan string),
-				timeout:     timer,
-				writer:      w,
-				stoptimer:   make(chan bool),
-				expired:     make(chan bool),
-				done:        make(chan bool),
-				stopped:     false,
-			}
-			// Send GetRequest to Broker's common channel for all GetRequests
-			go br.AddGetRequest(me)
-			// Start timeout countdown for the GetRequest
-			go me.Wait()
-			// this select awaiting one of possible result of the GetRequest's processing
-			select {
-			case response := <-me.DataChannel: // in case of data received, reply 200 OK and send data
-				me.Reply(response)
-			case <-me.done: // in case of no data was received, reply 404
-				me.Decline()
-			case <-me.expired: // in case of time is up, set GetRequest.stopped = true and reply 404
-				me.Stop()
-				me.Decline()
-			case <-r.Context().Done(): // in case of sudden disconnection, set GetRequest.stopped = true
-				me.Stop()
-			}
-		}
-	})
+	http.HandleFunc("/", HandleRequest)
 
 	var ListenPort string
 	flag.StringVar(&ListenPort, "port", "80", "Http port where to serve requests")
